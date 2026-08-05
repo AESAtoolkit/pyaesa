b9cf9630a03743a754db2e832e686336cc92faa4
IKE-EDB <erwandebantel@hotmail.com>
2026-08-05 18:52:02 +0200

    update version name

--- pyaesa/__init__.py
+++ pyaesa/__init__.py
@@ -46,7 +46,7 @@ Public functions exported at package level:
 from importlib import import_module
 from typing import TYPE_CHECKING
 
-__version__ = "1.2.5"
+__version__ = "1.2.6"
 
 _PUBLIC_EXPORT_OWNERS = {
     "set_workspace": "pyaesa.workspace_initialisation.set_workspace",
